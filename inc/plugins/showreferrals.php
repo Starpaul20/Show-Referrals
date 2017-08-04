727154554492238e369703ee89903242840f4564
PaulBender <paulbender@live.com>
2017-08-04 15:00:17 -0400

    Released plugin

--- inc/plugins/showreferrals.php
+++ inc/plugins/showreferrals.php
@@ -40,6 +40,7 @@ function showreferrals_info()
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
 		"version"			=> "1.0",
+		"codename"			=> "showreferrals",
 		"compatibility"		=> "18*"
 	);
 }
